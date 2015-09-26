82c219a0d932dcf2eec113554568f10bc5966996
cheapbrain <ermarigo96@gmail.com>
2015-09-26 22:51:00 +0200

    i grande I

--- desktop/src/com/placydia/aisuperfighter/desktop/DesktopLauncher.java
+++ desktop/src/com/placydia/aisuperfighter/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.placydia.aisuperfighter.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.placydia.aisuperfighter.FIghterMain;
+import com.placydia.aisuperfighter.FighterMain;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new FIghterMain(), config);
+		new LwjglApplication(new FighterMain(), config);
 	}
 }
