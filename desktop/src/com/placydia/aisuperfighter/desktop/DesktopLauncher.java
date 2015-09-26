4b7f997d11febb332d7ff64b3868526e4833e7b0
spruzzatore <lucagalofaro@outlook.it>
2015-09-26 22:46:08 +0200

    bordello

---
+++ desktop/src/com/placydia/aisuperfighter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.placydia.aisuperfighter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.placydia.aisuperfighter.FIghterMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FIghterMain(), config);
+	}
+}
