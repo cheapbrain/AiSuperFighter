e565e4d3189f13d26d3d924186f801c5dc234dda
cheapbrain <ermarigo96@gmail.com>
2015-09-26 22:45:40 +0200

    first commit

---
+++ desktop/src/com/placydia/aisuperfighter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.placydia.aisuperfighter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.placydia.aisuperfighter.FighterMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FighterMain(), config);
+	}
+}
